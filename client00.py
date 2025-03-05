8414f29361f416aa9b25d397ddb27b6264d68611
Michael-Aladejobi <mikkystechs@gmail.com>
2025-03-05 22:17:18 +0100

    ...

--- client00.py
+++ client00.py
@@ -1,15 +1,23 @@
 import socket
 
-cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+cs = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM)
+print('client start: ')
 
-print('Client Start: ')
-
-port = 9000
+port = 9001
 ip = '127.0.0.1'
+cs.connect((ip, port))
 
-cs.connect((ip,port))
 
-msg = input('Enter the message: ')
+# sending msg to server using TCP 
+msg = input('message to server: ')
 cs.sendall(bytes(msg.encode('ascii')))
-print('Message sent to server...')
-cs.close()
+
+data = cs.recv(1024).decode()
+print('message from server ', data)
+
+msg = input('message to server: ')
+while msg.lower().strip() != 'bye':
+    cs.sendall(bytes(msg.encode('ascii')))
+    data = cs.recv(1024).decode()
+    print('message from server ', data)
+    msg = input('message time or type (bye) to quit: ')
--- server00.py
+++ server00.py
@@ -1,24 +1,31 @@
 import socket
+import time
 
-ss = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+ss = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM)
+print('server start: ')
 
-print('Server Start: ')
-
-port = 9000
+# Assigning port number
+port = 9001
 ip = '127.0.0.1'
-
-ss.bind((ip,port))
-
-ss.listen(2)
-
-# to receive from client
+ss.bind((ip, port))
+ss.listen(2) # the 2 here is the number of client the server will listen to at the same time maximum is 5
 con, addr = ss.accept()
+print('address client connected {0} and port number {1}'.format(addr[0], addr[1]))
 
-print('Addressssss Client Connected {} and port no {}'.format(addr[0], addr[1]))
+data = con.recv(2020)
+print('message from client before decoding: ', data)
 
-data = con.recv(1024)
-print('Data received from client: ', data.decode('ascii'))
+data = data.decode()
+print('message from client after decoding: ', data)
 
-con.close()
-ss.close()
-print('Server closed')
+msg = input('message to client: ')
+con.sendall(bytes(msg.encode('ascii')))
+
+while True:
+    data = con.recv(2020).decode()
+    if not data:
+        print('end time request')
+        break
+    print('message from client: ', data)
+    mg = time.ctime(time.time())+'\r\n'
+    con.sendall(bytes(mg.encode('ascii')))
