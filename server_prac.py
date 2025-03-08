c4bea20bfd90b0345bd727a5a46cb87793a298da
Michael-Aladejobi <mikkystechs@gmail.com>
2025-03-08 21:59:02 +0100

    ,,,

--- server_prac.py
+++ server_prac.py
@@ -1,29 +1,14 @@
 import socket
 
+ss=socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
-serverSide = socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM)
-
-
-print('Server Ready: ')
+print('Server start: ')
 
 port = 9000
 ip = '127.0.0.1'
 
-serverSide.bind((ip, port))
-serverSide.listen()
-con, addr = serverSide.accept()
+ss.bind((ip, port))
+ss.listen()
 
-print('Connected to Address {0} and Port no. {1}'.format(addr[0], addr[1]))
-
-data = con.recv(1024)
-
-
-while True:
-    if not data:
-        print('Session ended by Client!')
-        break
-    else:
-        print('Client data before decoding: ', data)
-        data = data.decode()
-        print('Client data after decoding: ', data)
-        data = con.recv(1024)
+con, addr = ss.accept()
+print(f'Connected to addres:{addr[0]}, port:{addr[1]}')
