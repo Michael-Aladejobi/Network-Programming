9e2d1bd6822c4c48d8dbdaa1589cf56be75df1a6
Michael-Aladejobi <mikkystechs@gmail.com>
2025-03-08 17:59:50 +0100

    ..

--- server_prac.py
+++ server_prac.py
@@ -1,33 +1,6 @@
 import socket
 
-server_side = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
-print('Server Ready: ')
+serverSide = socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM)
 
-port = 9000
-ip = '127.0.0.1'
 
-server_side.bind((ip, port))
-server_side.listen(1)
-
-con, addr =  server_side.accept()
-
-print('Address clien connected {0} and port no. {1}'. format(addr[0], addr[1]))
-
-# data = con.recv(1024)
-# print('Message from client before decode: ', data)
-
-# data = data.decode()
-# print('Message from client after decode', data)
-
-data = con.recv(1024)
-while True:
-    if not data:
-        print('Session ended by Client!')
-        break
-    else:
-        print("Message from client before decoding: ", data)
-
-        data = data.decode()
-        print('Message from client after decoding: ', data)
-        data = con.recv(1024)
