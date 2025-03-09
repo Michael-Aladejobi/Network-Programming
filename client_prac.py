35b1d237869b87a041495da5f0a30c7269208d3b
Michael-Aladejobi <mikkystechs@gmail.com>
2025-03-09 21:28:29 +0100

    ///

--- client_prac.py
+++ client_prac.py
@@ -1,5 +1,6 @@
 import socket
-cs = socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM)
+
+cs = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM)
 
 print('Client start: ')
 
@@ -8,24 +9,20 @@ ip = '127.0.0.1'
 
 cs.connect((ip, port))
 
-msg = input('Message to serer: ')
+msg = input("Message to server: ")
 cs.sendall(bytes(msg.encode('ascii')))
 
 data = cs.recv(1024)
 print('Message from server before decode: ', data)
-
 data = data.decode()
 print('Message from server after decode: ', data)
 
 msg = input('Message to server: ')
-while True:
-    if msg.upper() == 'QUIT':
-        print('Session ended by Client!')
-        break
-    else:
-        cs.sendall(bytes(msg.encode('ascii')))
-        data = cs.recv(1024).decode()
-        print('Message recv from server: ', data)
-        msg = input('Message to server: ')
-
-
+while msg.lower().strip() != 'bye':
+    cs.sendall(bytes(msg.encode('ascii')))
+    data = cs.recv(1024)
+    print('Messagr from server before decode: ', data)
+    data = data.decode()
+    print('Message from sever after decode: ', data)
+    msg = input('Message to server or type (bye) to quit: ')
+   
--- server_prac.py
+++ server_prac.py
@@ -1,6 +1,6 @@
 import socket
 
-ss=socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+ss = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM)
 
 print('Server start: ')
 
@@ -8,28 +8,28 @@ port = 9000
 ip = '127.0.0.1'
 
 ss.bind((ip, port))
-ss.listen()
+ss.listen(2)
 
 con, addr = ss.accept()
-print(f'Connected to addres:{addr[0]}, port:{addr[1]}')
+
+print('Connected to address: {0}, port no.: {1}'.format(addr[0], addr[1]))
 
 data = con.recv(1024)
 print('Message from client before decode: ', data)
-
 data = data.decode()
 print('Message from client after decode: ', data)
+
 
 msg = input('Message to client: ')
 con.sendall(bytes(msg.encode('ascii')))
 
-msg = input('Message to client: ')
 while True:
+    data = con.recv(1024)
     if not data:
         print('Session ended by client!')
         break
-    con.sendall(bytes(msg.encode('ascii')))
-    data = con.recv(1024).decode()
-    print('Message recv from client: ', data)
-
-   
+    print('Message from client before decode: ', data)
+    Sdata = data.decode()
+    print('Message from client after decode: ', data)
     msg = input('Message to client: ')
+    con.sendall(bytes(msg.encode('ascii')))
