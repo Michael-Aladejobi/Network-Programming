221f39d0879eea92a58b3031e1d4ca9c02789a55
Michael-Aladejobi <mikkystechs@gmail.com>
2025-03-24 10:30:16 +0100

    ...

--- 3rd class/server00.py
+++ 3rd class/server00.py
@@ -1,3 +1,19 @@
 import socket
-
 from _thread import *
+
+def fun(ss2):
+    pass
+
+ss = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM)
+
+print('Server Start: ')
+host = socket.gethostname()
+port = 6000
+
+ss.bind((host, port))
+ss.listen(4)
+
+while True:
+    ss2, addr = ss.accept()
+    print('Address client {0} and por no. {1}'.format(addr[0], addr[1]))
+    start_new_thread(fun, (ss2,))
