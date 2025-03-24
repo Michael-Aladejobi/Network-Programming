a316177ecd93c7f2e2750ce638d11bd7c2608a4e
Michael-Aladejobi <mikkystechs@gmail.com>
2025-03-24 10:41:07 +0100

    client counter

--- 3rd class/server00.py
+++ 3rd class/server00.py
@@ -1,5 +1,8 @@
 import socket
 from _thread import *
+import os
+
+th = 0 #Thread counter var
 
 def fun(ss2):
     pass
@@ -17,3 +20,5 @@ while True:
     ss2, addr = ss.accept()
     print('Address client {0} and por no. {1}'.format(addr[0], addr[1]))
     start_new_thread(fun, (ss2,))
+
+    th = th + 1 #client counter
