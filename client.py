7cd9a1e7c2fc274068876fe57eeb5717be4011d6
Michael-Aladejobi <mikkystechs@gmail.com>
2025-04-01 22:44:35 +0100

    ....

--- client.py
+++ client.py
@@ -1,15 +1,17 @@
 import socket
 
-cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+cs = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM)
 
-print("Client Start: ")
-host = "127.0.0.1"
-port = 7000
+print('Client Strat: ')
+
+host = '127.0.0.1'
+port = 6000
 
 cs.connect((host, port))
 
-msg = input("Message to server: ")
-cs.sendall(bytes(msg.encode('ascii')))
+msg = input('message to server: ')
+cs.sendall(msg.encode('ascii'))
 
-data = cs.recv(1024).decode('ascii')
-print('Message from srver: ', data)
+data = cs.recv(1024).decode()
+print('message from server: ', data)
+
--- server.py
+++ server.py
@@ -1,19 +1,18 @@
-import socket;
+import socket
 
-ss = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-
-print(" Server Start: ")
-host = "127.0.0.1"
-port = 7000
+ss = socket.socket(family = socket.AF_INET , type = socket.SOCK_STREAM)
+print('Server Start: ')
+host = '127.0.0.1'
+port = 6000
 
 ss.bind((host, port))
 ss.listen(5)
 con, addr = ss.accept()
 
-print("Connected to Address: {0}, port no.: {1}".format(addr[0],addr[1]))
+print('Connected to Address:{0}, port no.: {1}'.format(addr[0], addr[1]))
 
 data = con.recv(1024).decode()
-print('message recv from client: ', data)
+print('message from client: ', data)
 
-msg = input('Message to client: ')
+msg = input('message to client: ')
 con.sendall(bytes(msg.encode('ascii')))
