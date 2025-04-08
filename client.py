10dadf9c177577ef62682b64608a9e9cb120de8f
Michael-Aladejobi <mikkystechs@gmail.com>
2025-04-08 01:56:51 +0100

    ...

--- client.py
+++ client.py
@@ -1,27 +1,31 @@
 import socket
 
-cs = socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM)
 
-print('client start: ')
-
+cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+print('Client start: ')
 host = socket.gethostname()
-port = 9999
-
+port = 3000
 cs.connect((host, port))
-
-msg = input('message to server: ')
-cs.sendall(bytes(msg.encode('ascii')))
+msg = input('message to server : ')
+cs.send(bytes(msg.encode('ascii')))
 
 data = cs.recv(1024).decode()
-print('message from server: ', data)
+print('message from server : ',data)
 
-msg = input('message to server: ')
-while True:
-    if msg.lower().strip() != 'bye':
-        cs.sendall(bytes(msg.encode('ascii')))
-        data =  cs.recv(1024).decode()
-        print('message from server: ', data)
-        msg = input('message to sever or enter (bye to quit: )')
+msg = input('message to server : ')
+while msg.lower() != 'bye':
 
+    cs.send(bytes(msg.encode('ascii')))
+
+    data = cs.recv(1024).decode()
+    print('message from server : ',data)
+
+    data = cs.recv(1024).decode()
+    print('message from server : ',data)
+    msg = input('message 1 to 9 to server or type (bye) to quit : ')
 cs.close()
-        
+
+
+
+
+
--- server.py
+++ server.py
@@ -1,40 +1,74 @@
 import socket
 from _thread import *
 import os
+import random
 
-th = 0 #thread var counter
+th = 0
+sa = 0
+ca = 0
+da = 0
+
+def stat():
+    global sa, ca, da
+    res = 'statistical analysis: '
+    res = res + f'server won with {sa}, client won with {ca} !'
+    return res
+
+def whoWon(s, c):
+    global sa, ca, da
+    res = ''
+    if c > s:
+        res = res + f'client won with {c} as against server with {s}'
+        ca = ca + 1
+        return res
+    elif c == s:
+        res = res + f'draw:  client with {c} as against server { s}'
+        da = da + 1
+        return res
+    else:
+        res = res + f'server won with {s} as against client {c}'
+        sa = sa + 1
+        return res
+    
+def randomNo():
+    res = random.randint(1, 9)
+    return res
 
 def func(con):
     data = con.recv(1024).decode()
     print('message from client: ', data)
 
-    msg = input('mesage to client: ')
+    msg = input( 'message to client: ')
+    con.sendall(msg.encode('ascii'))
+
     while True:
-        if msg.lower().strip() != 'bye':
-            con.sendall(msg.encode('ascii'))
+        data = con.recv(1024).decode()
+        if not data:
+            print('session ended')
+        clientNo = int(data)
+        serverNo = randomNo()
+        msg = 'server No: {0}, client No: {1}'.format(clientNo, serverNo)
+        print(msg)
+        con.sendall(msg.encode('ascii'))
 
-            data = con.recv(1024).decode()
-            print('message from client: ', data)
+        res = whoWon(serverNo, clientNo)
+        print(res)
+        con.sendall(res.encode('ascii'))
 
-            msg = input('message to client: ')
+        stats = stat()
+        print(stats)
+        con.sendall(stats.encode('ascii'))
 
 
+ss = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM)
 print('server start: ')
-
-ss = socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM)
-
 host = socket.gethostname()
-port = 9999
-
+port = 3000
 ss.bind((host, port))
-ss.listen(5)
-
+ss.listen(4)
 while True:
     con, addr = ss.accept()
-    print('Connected to addr {0}, port no. {1}'.format(addr[0], addr[1]))
-
-    start_new_thread(func,(con,))
+    print('address client {0} and port no {1}'.format(addr[0], addr[1]))
+    start_new_thread(func, (con,))
     th = th + 1
-    print('thread no.: ', th)
-
-    print('process id: '.format(os.getpid()))
+    print('thread no {0} and process id {1}'.format(th, os.getpid()))
