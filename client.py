8075fd12eb2966c3f78609063835a45760256ffb
Michael-Aladejobi <mikkystechs@gmail.com>
2025-04-10 07:04:23 +0100

    ...

--- client.py
+++ client.py
@@ -1,1 +1,25 @@
-import
+import socket
+
+cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+print('client start: ')
+
+host = socket.gethostname()
+port = 9999
+
+cs.connect((host, port))
+
+msg = input('message to server: ')
+cs.sendall(bytes(msg.encode('ascii')))
+
+data = cs.recv(1024).decode()
+print('message from server: ', data)
+
+msg = input('message to server: ')
+while True:
+    if msg.lower().strip() != 'bye':
+        cs.sendall(bytes(msg.encode('ascii')))
+        data = cs.recv(1024).decode()
+        print('message from client: ', data)
+        msg = input('message to server: ')
+
+cs.close()
