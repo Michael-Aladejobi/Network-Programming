af4cf27f5d17a65d1ba0f86de7f24b45868d4f0d
Michael-Aladejobi <mikkystechs@gmail.com>
2025-04-13 01:34:24 +0100

    ...

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 print('client start: ')
 
 host = socket.gethostname()
-port = 9999
+port = 9000
 
 cs.connect((host, port))
 
@@ -24,6 +24,7 @@ while True:
 
         msg = input('message to server or type (bye) to quit: ')
     else:
+        
         print('proces terminated!')
         break
 
