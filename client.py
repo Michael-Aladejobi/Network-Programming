56781abc12bfb79dfc02fa1174630c32c8e353a3
Michael-Aladejobi <mikkystechs@gmail.com>
2025-04-13 02:39:16 +0100

    ...

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+
+cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+print('Client Start: ')
+
+host = socket.gethostname()
+port = 9999
+
+msg = input('message to server: ')
+msg = str.encode(msg)
+cs.sendto(msg, (host, port))
+
+
+con, addr = cs.recvfrom(1024)
+data = con.decode()
+print('message from server: ', data)
+
+msg = input('message to server: ')
+while True:
+    if msg.lower().strip() != 'bye':
+        msg = str.encode(msg)
+        cs.sendto(msg, (host, port))
+
+        con, addr = cs.recvfrom(1024)
+        data = con.decode()
+        print('message from server: ', data)
+
+        msg = input('message to server or type (bye to quit): ')
--- server.py
+++ server.py
@@ -8,4 +8,18 @@ host = socket.gethostname()
 port = 9999
 
 ss.bind((host, port))
-con, addr = ss.recvfrom() 
+con, addr = ss.recvfrom(1024)
+
+print('Connected to addr: {0}'.format(addr))
+
+data = con.decode()
+print('message from client: ', data)
+
+msg = input('message to client: ')
+msg = str.encode(msg)
+ss.sendto(msg, (addr))
+
+while True:
+    con, addr = ss.recvfrom(1024)
+    data = con.decode()
+    print('message from client: ', data)
