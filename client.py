b6d293c3470b6282e4362c5faf99253670ffc6c2
Michael-Aladejobi <mikkystechs@gmail.com>
2025-04-14 09:16:16 +0100

    ...

--- client.py
+++ client.py
@@ -14,3 +14,10 @@ con, addr = cs.recvfrom(1024)
 data = con.decode()
 print('messqge from server: ', data)
 
+msg = input('message to server as a _ b _ c: ')
+msg = str.encode(msg)
+cs.sendto(msg, (host, port))
+
+con, addr = cs.recvfrom(1024)
+data = con.decode()
+print('message from server: ', data)
