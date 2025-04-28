863ba8c8241ac4b0e4cc7840163c264e8d33afb6
Michael-Aladejobi <mikkystechs@gmail.com>
2025-04-28 23:12:13 +0100

    con.recv(1024).decode()

--- 2nd class/server00.py
+++ 2nd class/server00.py
@@ -6,15 +6,12 @@ import os
 
 th = 0 #counter var
 
-
 def func(c):
     data = c.recv(1024).decode()
     print('message from client : ', data)
     
-
 ss = socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM)
 print("Start Server: ")
-
 
 host = socket.gethostname()
 port = 9500 
@@ -26,7 +23,6 @@ while True:
     con, addr = ss.accept()
     print("address client conn. {} and port no. {}".format (addr[0], addr[1]))
     start_new_thread(func, (con,))
-
 
     th = th + 1
     print('client thread ', th)
