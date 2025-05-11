3755408a8d1e1c4390027e42f2cc17faf2b66eac
Michael-Aladejobi <mikkystechs@gmail.com>
2025-05-11 12:21:05 +0100

    con.recv(1024).decode()

--- 7th classssss/UDP/client.py
+++ 7th classssss/UDP/client.py
@@ -1,52 +1,54 @@
 import socket
 import pickle
 
-cs = socket.socket()
-print('Client starting...')
 
-host = '127.0.0.1'
-port = 7001
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ("127.0.0.1", 7001)
 
-cs.connect((host, port))
+print("Client started...")
 
-msg = input('Message to server: ')
-cs.send(msg.encode())
+msg = input('Say hello to server: ')
+msg = str.encode(msg)
+client_socket.sendto(msg, server_address)
 
-data = cs.recv(1024).decode()
-print('Server response:', data)
 
-data = cs.recv(1024)
-arr = pickle.loads(data)
-print('Game array:', arr)
+game_array_data, _ = client_socket.recvfrom(1024)
+game_array = pickle.loads(game_array_data)
+print("Game array:", game_array)
 
-while True:
-    index = input('Enter index (0-3) or "bye": ')
-    if index == 'bye':
-        cs.send(b'bye')
+selected_index = input("Enter an index from 0 - 3: ")
+while selected_index.strip().lower() != "bye":
+    client_socket.sendto(selected_index.encode(), server_address)
+
+    selected_value = input("Enter a value from 1 - 9: ")
+    client_socket.sendto(selected_value.encode(), server_address)
+
+    updated_game_array, _ = client_socket.recvfrom(1024)
+    if updated_game_array == b"win":
+        print("Congratulations! You won!")
         break
-    
-    value = input('Enter value (1-9): ')
-    
-    cs.send(index.encode())
-    cs.send(value.encode())
-    
-    data = cs.recv(1024)
-    if data == b'win':
-        print('You won!')
-        break
-    
-    arr = pickle.loads(data)
-    print('After your move:', arr)
-    
-    data = cs.recv(1024)
-    if data == b'win':
-        print('Server won!')
-        break
-    
-    arr = pickle.loads(data)
-    print('After server move:', arr)
 
-cs.close()
+    game_array = pickle.loads(updated_game_array)
+    print("Game array after your move:", game_array)
+
+    updated_game_array, _ = client_socket.recvfrom(1024)
+    if updated_game_array == b"win":
+        print("Client wins! Game over.")
+        break
+
+    game_array = pickle.loads(updated_game_array)
+    print("Game array after server's move:", game_array)
+
+    if all(element == 0 for element in game_array):
+        print("Server wins! Game over.")
+        break
+
+    selected_index = input("Enter an index from 0 - 3 or type 'bye' to quit: ")
+
+client_socket.sendto(b"bye", server_address)
+client_socket.close()
+
+
 
 
 
--- 7th classssss/UDP/server.py
+++ 7th classssss/UDP/server.py
@@ -3,112 +3,106 @@ import random
 import pickle
 import threading
 
-arr = []
+
+array = []
 N = 4
 M = 9
 
+
 def genArray():
-    global arr
-    arr = []
+    
     for i in range(N):
         x = random.randint(1, M)
-        arr.append(x)
+        array.append(x)
+
 
 def genIndex():
     x = random.randint(0, N-1)
     return x
 
+
 def genValue():
     x = random.randint(1, M)
     return x
 
-def pop(arr, N, index, value):
-    if index < 0 or index >= N:
+
+def pop(array, N, index, value):
+    if ((index) < 0 or (index > N)):
         print('Index Error')
         return False
-    if value > arr[index] or value < 0:
+    if ((value > array[index]) or (value < 0)):
         print('Impossible to subtract less value')
-        return False
+        return False #[4,3,2,1] - [5, 2 2 1] = error
     else:
-        arr[index] -= value
+        array[index] = array[index] - value
         return True
+
 
 def isWinner(arr):
     return all(x == 0 for x in arr)
 
-def handle_client(con, addr):
-    print(f'Client connected: {addr}')
-    
-    # Initial message
-    data = con.recv(1024)
-    print('Client message:', data.decode())
-    
-    con.send(b'Game started')
-    
-    # Generate and send array
-    genArray()
-    print('Game array:', arr)
-    con.send(pickle.dumps(arr))
-    
-    while True:
-        # Client move
-        indexCli = int(con.recv(1024).decode())
-        valueCli = int(con.recv(1024).decode())
-        
-        # Server move
-        indexSer = genIndex()
-        valueSer = genValue()
-        
-        print(f'Client move: index {indexCli}, value {valueCli}')
-        print(f'Server move: index {indexSer}, value {valueSer}')
-        
-        # Process client move
-        if pop(arr, N, indexCli, valueCli):
-            print('After client move:', arr)
-            con.send(pickle.dumps(arr))
-        else:
-            con.send(pickle.dumps(arr))
-            continue
-        
-        # Check winner
-        if isWinner(arr):
-            con.send(b'win')
-            print('Client won')
-            break
-        
-        # Process server move
-        if pop(arr, N, indexSer, valueSer):
-            print('After server move:', arr)
-            con.send(pickle.dumps(arr))
-        else:
-            con.send(pickle.dumps(arr))
-            continue
-        
-        # Check winner
-        if isWinner(arr):
-            con.send(b'win')
-            print('Server won')
-            break
-    
-    con.close()
 
-# Main server code
-ss = socket.socket()
-print('Server starting...')
+def func(con, addr, server_socket):
 
-host = '127.0.0.1'
-port = 7001
+    global array
 
-ss.bind((host, port))
-ss.listen(5)
-print(f'Server listening on {host}:{port}')
+    if con.lower().strip() == "hello":
+
+         # Send game array
+        server_socket.sendto(pickle.dumps(array), addr) 
+        return
+
+    # Ensure received data is numeric before processing
+    if not con.isdigit():
+        print(f"Invalid input from {addr}: {con}")
+        return
+
+    client_index = int(con)
+    client_value = server_socket.recvfrom(1024)[0].decode()
+    client_value = int(client_value)
+
+    server_index = genIndex()
+    server_value = genValue()
+
+    print(f"Client {addr} chose index {client_index} with value {client_value}")
+    print(f"Server chose index {server_index} with value {server_value}")
+
+    
+    if pop(array, N, client_index, client_value):
+        print("Game array after client's move:", array)
+        server_socket.sendto(pickle.dumps(array), addr)
+    else:
+        server_socket.sendto(pickle.dumps(array), addr)
+
+    
+    if isWinner(array):
+        server_socket.sendto(b"win", addr)
+        print("Client wins the game!")
+        return
+
+    
+    if pop(array, N, server_index, server_value):
+        print("Game array after server's move:", array)
+        server_socket.sendto(pickle.dumps(array), addr)
+    else:
+        server_socket.sendto(pickle.dumps(array), addr)
+
+    
+    if isWinner(array):
+        server_socket.sendto(b"win", addr)
+        print("Server wins the game!")
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_host = "127.0.0.1"
+server_port = 7001
+server_socket.bind((server_host, server_port))
+
+print("Server Start...")
+
+genArray()
+print("Initial game array:", array)
 
 while True:
-    con, addr = ss.accept()
-    print(f'New connection from {addr}')
-    thread = threading.Thread(target=handle_client, args=(con, addr))
-    thread.start()
-
-
-
-
+    con, addr = server_socket.recvfrom(1024)
+    threading.Thread(target=func, args=(con.decode(), addr, server_socket)).start()
