932346ed36004c35ed57d728d31ee93941f1d54b
Michael-Aladejobi <mikkystechs@gmail.com>
2025-05-22 23:24:12 +0100

    con.recv(1024).decode()

--- 1st class/server_prac.py
+++ 1st class/server_prac.py
@@ -1,33 +1,34 @@
 import socket
 import time
 
-ss = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Server start: ')
 
-host = socket.gethostname()
+host = '127.0.0.1'
 port = 6000
 
 ss.bind((host, port))
 ss.listen(5)
-con, addr = ss.accept()
 
-print(f'Connected to Addr: {addr[0]}, port no: {addr[1]}')
+con, addr = ss.accept()
+print('Connected to addr : {0}, port: {1}'.format(addr[0], addr[1]))
 
 data = con.recv(1024)
-print('data b/f decoding: ', data)
+print('msg from client b/f decoding: ', data)
 data = data.decode()
-print('data after decoding: ', data)
+print('msg from client a/f decoding: ', data)
 
-msg = input('message to client to request time: ')
-con.sendall(bytes(msg.encode('ascii')))
+msg = input(' message to client: ')
+msg = msg.encode('ascii')
+con.sendall(bytes(msg))
 
 while True:
-    data = con.recv(1024).decode()
-    if data == 'bye':
-        print('Session ended!')
-        break
+    data = con.recv(1024)
     if not data:
-        print('Session ended')
+        print('end time request')
+        break
+    if data.lower().strip() == 'bye':
+        print('end time request')
         break
     print('message from client: ', data)
 
