3d47168c32d538171a9423a6eddabee36f83e2a2
Michael-Aladejobi <mikkystechs@gmail.com>
2025-05-26 09:07:01 +0100

    con.recv(1024).decode()

---
+++ MY GAME/TCP/client_prac.py
@@ -0,0 +1,27 @@
+import socket
+
+cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+print('client start: ')
+
+host = socket.gethostname()
+port = 6000
+
+cs.connect((host, port))
+
+data = cs.recv(1024).decode()  
+print('message from server: ', data)
+
+msg = input('message to server: ')
+while True:
+    if msg.lower().strip() != 'bye':
+        cs.sendall(bytes(msg.encode('ascii')))
+
+        data = cs.recv(1024).decode()
+        print('message from server-->: ', data)
+        
+        msg = input('message to server: ')
+    else:
+        print("Exiting game...")
+        break
+
+cs.close()
--- MY GAME/TCP/server_prac.py
+++ MY GAME/TCP/server_prac.py
@@ -32,11 +32,81 @@ def hot_or_cold(treasure, guess):
         msg = "Cold ( Close)"
         return msg
 
-def func:
-    pass
+def func(con):
+    global guess_count 
+    global max_guesses
+
+    treasure = get_treasure()
+
+    msg = "Welcome to Hot or Close Treasure Hunt. You only have 5 guesses. Good Luck!"
+    con.sendall(bytes(msg.encode('ascii')))
+
+    while True:
+        data = con.recv(1024)
+        data = data.decode()
+
+        if not data:
+            print("Game over")
+            break
+        if data.lower().strip() == 'bye':
+            print("Game Over")
+            break
+
+        print('message from client :', data)
+
+        guess_count = guess_count + 1
+
+        try:
+            client_guess = int(data)
+            if client_guess < 1 or client_guess > 50:
+                msg = "Out of range. Enter btw 1 and 50"
+                print("Client side: ", msg)
+                con.sendall(bytes(msg.encode('ascii')))
+                continue
+        except ValueError:
+            msg = "Invalid Valid. Enter a valid value."
+            print('Client side: ', msg)
+            con.sendall(bytes(msg.encode('ascii')))
+        
+        if client_guess == treasure:
+            msg = "Congratulations! Client found the treasure."
+            print(msg)
+            con.sendall(bytes(msg.encode('ascii')))
+            break
+        elif client_guess >= guess_count:
+            msg = "Out of Guess. Server Won!"
+            print(msg)
+            con.sendall(bytes(msg.encode('ascii')))
+            break
+        else:
+            remaining_guesses = max_guesses - guess_count
+            result = hot_or_cold(treasure, client_guess)
+            result = result + f""
+            print(result)
+            con.sendall(bytes(result.encode('ascii')))
+
+            move_treasure(treasure)
+
+    
+
+
 
 ss = socket.socket(family=socket.AF_INET, type = socket.SOCK_STREAM)
 print('Server start: ')
 
 host = socket.gethostname()
 port = 6000
+
+ss.bind((host, port))
+ss.listen(5)
+
+while True:
+    con, addr = ss.accept()
+    print("Connected to addr {0}, port {1}".format(addr[0], addr[1]))
+
+    start_new_thread(func, (con,))
+
+    th = th + 1
+    print("Thread no.: ", th)
+
+    print("Process ID: ", os.getpid())
